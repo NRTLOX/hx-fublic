3a0db847c585b36ecc88fc1220799dd2f5ba9f6d
root <root@djangoOsvobozjdeniy.usue.ru>
2026-05-12 09:06:18 +0000

    before black

--- vms/admin.py
+++ vms/admin.py
@@ -7,3 +7,14 @@ class UserVMInstanceAdmin(admin.ModelAdmin):
     list_filter = ['status', 'task']
     search_fields = ['user__username', 'ip_address', 'task__title']
     readonly_fields = ['proxmox_vm_id', 'started_at', 'expires_at']
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['monitoring_link'] = get_monitoring_link()
+        return super().changelist_view(request, extra_context=extra_context)
+
+from django.urls import reverse
+from django.utils.html import format_html
+
+def get_monitoring_link():
+    url = reverse('admin_monitoring')
+    return format_html('<a href="{}" class="btn btn-primary" style="margin-bottom:10px;">📊 Мониторинг системы</a>', url)
