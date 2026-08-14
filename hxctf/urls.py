4b9f76f48f7f34f6a0747a177400a0786a4ca302
NRTLOX <Nert81i@ya.ru>
2026-08-14 09:12:38 +0500

    Add VM stop button to the admin monitoring dashboard's active VMs panel

--- hxctf/urls.py
+++ hxctf/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from vms.admin_dashboard import proxmox_dashboard
+from vms.admin_dashboard import proxmox_dashboard, admin_stop_vm
 
 urlpatterns = [
     path('admin/monitoring/', proxmox_dashboard, name='admin_monitoring'),
+    path('admin/monitoring/stop/<int:instance_id>/', admin_stop_vm, name='admin_stop_vm'),
 
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
